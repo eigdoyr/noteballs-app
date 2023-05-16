ac92c74e8c278d41e200673478523d2a96b4bd1c
ryodgie <ryodgie@gmail.com>
2023-05-16 16:29:01 +0800

    feat: add delete modal - del note

--- src/components/Notes/Note.vue
+++ src/components/Notes/Note.vue
@@ -13,17 +13,23 @@
         >Edit</RouterLink
       >
       <a
-        @click.prevent="storeNotes.deleteNote(note.id)"
+        @click.prevent="modals.deleteNote = true"
         href="#"
         class="card-footer-item"
         >Delete</a
       >
     </footer>
+    <ModalDeleteNote
+      v-if="modals.deleteNote"
+      v-model="modals.deleteNote"
+      :noteId="note.id"
+    />
   </div>
 </template>
 
 <script setup>
-import { computed } from "vue";
+import { computed, reactive } from "vue";
+import ModalDeleteNote from "@/components/Notes/ModalDeleteNote.vue";
 import { useStoreNotes } from "@/stores/storeNotes";
 
 /*
@@ -47,5 +53,13 @@ const characterLength = computed(() => {
   let length = props.note.content.length;
   let description = length > 1 ? "characters" : "character";
   return `${length} ${description}`;
+});
+
+/*
+  modals
+ */
+
+const modals = reactive({
+  deleteNote: false,
 });
 </script>
